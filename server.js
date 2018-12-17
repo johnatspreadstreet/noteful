cc87068e4acdf9ed773b0093f685f84c2ec9bcea
John Young <john.young@spreadstreet.io>
2018-12-17 15:09:31 -0500

    Added all notes and singular notes GET functionality

--- server.js
+++ server.js
@@ -11,6 +11,22 @@ app.use(express.static('public'));
 
 console.log('Hello Noteful!');
 
+// 1st GET request
+app.get('/api/notes', (req, res) => {
+  res.json(data);
+});
+
+// Return specific notes based on ID
+app.get('/api/notes/:id', (req, res) => {
+  const params = req.params;
+  const id = params.id;
+  
+  const note = data.find(item => item.id === Number(id));
+
+  console.log(note);
+  res.send(note);
+});
+
 // INSERT EXPRESS APP CODE HERE...
 app.listen(8080, function() {
   console.info(`Server listening on ${this.address().port}`);
