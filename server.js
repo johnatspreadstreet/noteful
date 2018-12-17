e24d953067a5d6578274b7b47eb2b7e0b48489d0
John Young <john.young@spreadstreet.io>
2018-12-17 14:48:13 -0500

    Prepared initial setup

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 'use strict';
 
+const express = require('express');
+const app = express();
+
 // Load array of notes
 const data = require('./db/notes');
+
+// Load static files
+app.use(express.static('public'));
 
 console.log('Hello Noteful!');
 
 // INSERT EXPRESS APP CODE HERE...
+app.listen(8080, function() {
+  console.info(`Server listening on ${this.address().port}`);
+}).on('error', function(err) {
+  console.error(err);
+});
