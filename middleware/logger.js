b160b1184133024a86768a758fc5a9ddeb3dfca7
John Young <john.young@spreadstreet.io>
2018-12-18 17:20:37 -0500

    Simplified logging because im a dum dum

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,24 +1,12 @@
 'use strict';
 
-function formatDate(d){
-  var year = d.getFullYear();
-  var month = addLeadingZero(d.getMonth()); // Returns index of month, ie 11 = December
-  var day = addLeadingZero(d.getDay()); // Returns day of the week for date, ie 0 = Monday
-  var hours = d.getHours();
-  var minutes = d.getMinutes();
-  var seconds = d.getSeconds();
-  return year + '-' + month + '-' + day + ' ' + hours + ':' + minutes + ':' + seconds;
-}
-
-function addLeadingZero(n){ 
-  return n < 10 ? '0'+n : ''+n; 
-}
-
 const logging = function(req, res, next) {
-  let date = new Date();
-  let displayDate = formatDate(date);
+  let now = new Date();
+  let date = now.toLocaleDateString();
+  let time = now.toLocaleTimeString();
+  // let displayDate = formatDate(date);
   const {method, originalUrl} = req;
-  console.log(displayDate, method, originalUrl);
+  console.log(date, time, method, originalUrl);
   next();
 };
 
